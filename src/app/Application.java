38e41caad0258d9a8cd41c13852188c5c5a3ba53
Paul Friedli <paul.friedli@edufr.ch>
2025-06-19 13:16:18 +0200

    Correction consigne et projet selon entretien

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,55 +1,45 @@
 package app;
 
-import models.Pilote;
-
 public class Application {
 
     public static void main(String[] args) {
 
+        // ---------------------------------------------------------------------------------------
         // Etape 1 : Création d'une voiture de Formule 1
         // ---------------------------------------------------------------------------------------
 
-        
         // ---------------------------------------------------------------------------------------
         // Etape 2 : Création d'un pilote de Formule 1 et afficher ses informations
         // ---------------------------------------------------------------------------------------
 
+        // ---------------------------------------------------------------------------------------
+        // Etape 3 : Afficher le Pilote
+        // ---------------------------------------------------------------------------------------
 
         // ---------------------------------------------------------------------------------------
-        // Etape 3 : Tests des méthodes de la classe Pilote
+        // Etape 4 : Tests des méthodes de la classe Pilote
         // ---------------------------------------------------------------------------------------
         // deposerPneuEnReserve() 5 fois de types différents
 
-
-        // retirerPneuEnReserve() 1 fois
-
+        // retirerPneuEnReserve() 1 fois et l'afficher
 
         // getNombrePneusEnReserve() et afficher le nombre de pneus en réserve
 
-
         // getPneusEnReserveSansTrous() et afficher les pneus en réserve
 
+        // afficherPressionMoyennePneusEnReserve() et afficher la pression moyenne des
+        // pneus
 
-        // calculerPressionMoyennePneusEnReserve() et afficher la pression moyenne des pneus 
-        // en réserve
-
-
-        // compterNombrePneusEnReserveDeType() et afficher le nombre de pneus en réserve 
+        // compterNombrePneusEnReserveDeType() et afficher le nombre de pneus en réserve
         // de type DUR_BLANC
 
+        // Créer les 3 pneus et ajout le lot de pneus pour le pilote et afficher réussi
+        // ou échoué
 
+        // Supprimer les pneus de pression inférieure à 1.4 et afficher le nombre de
+        // pneus supprimés
 
-        // Créer les 3 pneus et ajout le lot de pneus pour le pilote et afficher réussi ou échoué.
-
-
-
-        // Supprimer les pneus de pression inférieure à 1.4 et afficher le nombre de pneus supprimés
-
-
-        
         // ---------------------------------------------------------------------------------------
-        
-
 
     }
 
